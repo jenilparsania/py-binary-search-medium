e6d7ac306e842f1a3b4474fd2ef52030008c1fc1
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-09-08 14:37:24 -0300

    Square Root

---
+++ Square Root/by_logN.py
@@ -0,0 +1,60 @@
+"""
+roblem statement
+You are given a positive integer ‘n’.
+
+
+
+Your task is to find and return its square root. If ‘n’ is not a perfect square, then return the floor value of sqrt(n).
+
+
+
+Example:
+Input: ‘n’ = 7
+
+Output: 2
+
+Explanation:
+The square root of the number 7 lies between 2 and 3, so the floor value is 2.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+6
+Sample Output 1:
+2
+Explanation of Sample Input 1:
+The square root of the given number 6 lies between 2 and 3, so the floor value is 2.
+Sample Input 2:
+100
+Sample Output 2:
+10
+Explanation of Sample Output 2:
+The square root of the given number 100 is 10.
+Expected Time Complexity:
+Try solving this in O(log(n)).
+Constraints:
+0 <= n <= 10 ^ 9
+
+Time Limit: 1 sec.
+
+"""
+def floorSqrt(n):
+   # write your code logic here .
+   high = n
+   low = 0
+   ans = 0
+   while low<=high:
+      mid = (low+high)//2
+      if mid**2==n:
+         return mid
+      if mid**2>=n:
+         high = mid -1
+         ans = mid
+      else:
+         low = mid +1
+   return high
+# n= int(input())
+# print(floorSqrt(n))
+
+n = 81
+print(floorSqrt(n))
